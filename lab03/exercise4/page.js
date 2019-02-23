0074c20747cce9576a6a72f7d754fddc3221d7b7
Evan Trout <fishyevan@gmail.com>
2019-02-23 11:00:13 -0600

    Finish styling of exercise4

--- lab03/exercise4/page.js
+++ lab03/exercise4/page.js
@@ -3,22 +3,38 @@ function editCSS() {
   let borderRed = document.getElementById("borderRed").value;
   let borderGreen = document.getElementById("borderGreen").value;
   let borderBlue = document.getElementById("borderBlue").value;
-  let newBorderColor = "rgb(" + borderRed + ", " + borderGreen + ", " + borderBlue + ")";
-  document.getElementById("fuck").style.borderColor = newBorderColor;
+  if (borderRed > 255 || borderGreen > 255 || borderBlue > 255)
+  {
+    alert("Please enter a valid color value (0-255)");
+  }
+  else if (borderRed < 0 || borderGreen < 0 || borderBlue < 0) {
+    alert("Please enter a valid color value (0-255)");
+  }
+  else {
+    let newBorderColor = "rgb(" + borderRed + ", " + borderGreen + ", " + borderBlue + ")";
+    document.getElementById("fuck").style.borderColor = newBorderColor;
+  }
   let backgroundRed = document.getElementById("backgroundRed").value;
   let backgroundGreen = document.getElementById("backgroundGreen").value;
   let backgroundBlue = document.getElementById("backgroundBlue").value;
-  let newBackgroundColor = "rgb(" + backgroundRed + ", " + backgroundGreen + ", " + backgroundBlue + ")";
-  document.getElementById("fuck").style.backgroundColor = newBackgroundColor;
-  //Base font color on background color; //(r * 0.299) + (g * 0.587) + (b * 0.114)) > 186
-  if ((Number(backgroundRed)*0.299) + (Number(backgroundGreen)*0.587) + (Number(backgroundBlue)*0.114) <= 150)
+  if (backgroundRed > 255 || backgroundGreen > 255 || backgroundBlue > 255)
   {
-    console.log("tan");
-    document.getElementById("fuck").style.color = "tan";
+    alert("Please enter a valid color value (0-255)");
+  }
+  else if (backgroundRed < 0 || backgroundGreen < 0 || backgroundBlue < 0) {
+    alert("Please enter a valid color value (0-255)");
   }
   else {
-    console.log("black");
-    document.getElementById("fuck").style.color = "black";
+    let newBackgroundColor = "rgb(" + backgroundRed + ", " + backgroundGreen + ", " + backgroundBlue + ")";
+    document.getElementById("fuck").style.backgroundColor = newBackgroundColor;
+    //Base font color on background color; //(r * 0.299) + (g * 0.587) + (b * 0.114)) > 186
+    if ((Number(backgroundRed)*0.299) + (Number(backgroundGreen)*0.587) + (Number(backgroundBlue)*0.114) <= 150)
+    {
+      document.getElementById("fuck").style.color = "tan";
+    }
+    else {
+      document.getElementById("fuck").style.color = "black";
+    }
   }
   return(false); //Why tf do i have to return false for this to work
 }
