caffccfce430e708be93f65fb70b0885d5260e42
Evan Trout <fishyevan@gmail.com>
2019-03-05 21:17:55 -0600

    Style ex2 and add return button

--- lab04/exercise2/quiz.php
+++ lab04/exercise2/quiz.php
@@ -17,19 +17,21 @@ $blackCard = $_POST["blackCard"];
 $checkBlackCard = "Flip a coin";
 $count = 0;
 
-echo "<p>Question 1: How many levels are in CharDee MacDennis?<br>";
+
+echo "<link href='style.css' type='text/css' rel='stylesheet'>";
+echo "<p><b>Question 1: How many levels are in CharDee MacDennis?</b><br>";
 echo "You answered: " . $levels . "<br>";
 echo "Correct answer: " . $checkLevels . "<br><br>";
-echo "<p>Question 2: What team has always lost the game?<br>";
+echo "<p><b>Question 2: What team has always lost the game?</b><br>";
 echo "You answered: " . $team . "<br>";
 echo "Correct answer: " . $checkTeam . "<br><br>";
-echo "<p>Question 3: What will Mac always try to do at some point during the game?<br>";
+echo "<p><b>Question 3: What will Mac always try to do at some point during the game?</b><br>";
 echo "You answered: " . $mac . "<br>";
 echo "Correct answer: " . $checkMac . "<br><br>";
-echo "<p>Question 4: What does Frank temporarily change the name of the game to?<br>";
+echo "<p><b>Question 4: What does Frank temporarily change the name of the game to?</b><br>";
 echo "You answered: " . $gameName . "<br>";
 echo "Correct answer: " . $checkGameName . "<br><br>";
-echo "<p>Question 5: What does the black card say teams must do in the event of a tie?<br>";
+echo "<p><b>Question 5: What does the black card say teams must do in the event of a tie?</b><br>";
 echo "You answered: " . $blackCard . "<br>";
 echo "Correct answer: " . $checkBlackCard . "<br><br></p>";
 
@@ -56,6 +58,6 @@ else {
   echo "<h3>You got " . $count . " correct answers!<h3>";
 }
 echo "<p>Score: " . calcScore($count) . "%</p>";
-
+echo "<a href='quiz.html'>Try again</a>"
 
 ?>
