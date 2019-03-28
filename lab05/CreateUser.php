11467d8cdd637f13f8104bd4c32679e52a43441d
Evan Trout <fishyevan@gmail.com>
2019-03-28 10:34:07 -0500

    Write first draft of exercise 1 (creating user)

---
+++ lab05/CreateUser.php
@@ -0,0 +1,28 @@
+<?php
+
+$username = $_POST["username"];
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "e106t851", "kahHohv3", "e106t851");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+  printf("Connect failed: %s\n", $mysqli->connect_error);
+  exit();
+}
+
+$query = "SELECT User_id FROM Users WHERE User_id = '$username'";
+
+if ($result = $mysqli->query($query)) {
+  echo '<script language="javascript">';
+  echo 'alert("ERROR: User already exists")';
+  echo '</script>';
+}
+else {
+  $query = "INSERT INTO Users (User_id) VALUES ('$username')";
+  $mysqli->query($query));
+  echo '<p>User successfully created!</p>';
+}
+
+$mysqli->close();
+
+?>
