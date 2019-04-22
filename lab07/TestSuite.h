ae4df009c4ce1bb98bdca52eb97c9589e4d7a94a
Evan Trout <fishyevan@gmail.com>
2019-04-22 17:30:52 -0500

    Add beginning of TestSuite

---
+++ lab07/TestSuite.h
@@ -0,0 +1,16 @@
+#ifndef TEST_SUITE_H
+#define TEST_SUITE_H
+
+#include "LinkedListOfInts.h"
+#include <iostream>
+#include <vector>
+
+class TestSuite 
+{
+private:
+public:
+    TestSuite(int testSize); //Constructor
+    void start(); //Begin testing suite
+};
+
+#endif
